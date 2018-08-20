e83eb9233583cc209ee2e102b463e2f3735a5272
Matheus Custodio Santos <44846245837@info.local>
2018-08-20 14:47:55 -0300

    Login terminado

--- php/login.php
+++ php/login.php
@@ -1,3 +1,10 @@
-?php>
-	
+<?php
+	$user =  $_POST["email_user"];
+	$pass =  $_POST["senha_passS"];
+
+	if($user == "matheus" && $pass == "02021996a"){
+		Parabéns!!! Você entrou!!!		
+	} else{
+		Seu Bosta!!!
+	}
 ?>
