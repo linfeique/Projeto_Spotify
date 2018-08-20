7846d9ec870207cbfa8a09682c9d59448ea93c17
Matheus Custodio Santos <44846245837@info.local>
2018-08-20 14:50:02 -0300

    Login De novo

--- php/login.php
+++ php/login.php
@@ -1,10 +1,23 @@
-<?php
-	$user =  $_POST["email_user"];
-	$pass =  $_POST["senha_passS"];
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Page Title</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+	<script src="main.js"></script>
+</head>
+<body>
+	<?php
+		$user =  $_POST["email_user"];
+		$pass =  $_POST["senha_passS"];
 
-	if($user == "matheus" && $pass == "02021996a"){
-		Parabéns!!! Você entrou!!!		
-	} else{
-		Seu Bosta!!!
-	}
-?>
+		if($user == "matheus" && $pass == "02021996a"){
+			Parabéns!!! Você entrou!!!		
+		} else{
+			Seu Bosta!!!
+		}
+	?>
+</body>
+</html>
